c5a62336303c537783377057f6434e6a93b0781e
vladislav-garaev <marker661@gmail.com>
2023-12-03 12:09:25 +0400

    feat: implement store check & error handling

--- connection/src/app/profile/services/profile-controller.service.ts
+++ connection/src/app/profile/services/profile-controller.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { BehaviorSubject, Observable, filter, finalize, map, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  catchError,
+  filter,
+  finalize,
+  map,
+  of,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { ProfileHttpService } from '../../api/profile.service';
 import { selectProfile } from '../../store/profile/profile.selectors';
 import { AppState, Profile } from '../../store/store.model';
 import { profileLoaded } from '../../store/profile/profile.actions';
 import { profileMapper } from '../hekpers/profile-mapper';
+import { NotificationService } from '../../core/services/notification.service';
+import { Notifications } from '../../api/consts/notifications';
 
 @Injectable({
   providedIn: 'root',
@@ -18,25 +30,29 @@ export class ProfileControllerService {
   constructor(
     private http: ProfileHttpService,
     private store: Store,
+    private notificationService: NotificationService,
   ) {}
 
   getProfile(): Observable<Profile> {
     return (this.store as Store<AppState>).select(selectProfile).pipe(
       filter(Boolean),
-      tap((profile) => {
-        if (!profile.uid) {
+      switchMap((profile) => {
+        if (profile.uid === '') {
           this.loading$$.next(true);
-          this.http
-            .getProfile()
-            .pipe(
-              map(profileMapper),
-              tap((profileData) => {
-                this.store.dispatch(profileLoaded({ profile: profileData }));
-              }),
-              finalize(() => this.loading$$.next(false)),
-            )
-            .subscribe();
+          return this.http.getProfile().pipe(
+            map(profileMapper),
+            tap((profileData) => {
+              this.store.dispatch(profileLoaded({ profile: profileData }));
+            }),
+            catchError((err) => {
+              this.notificationService.error(err.error.message || Notifications.UNKNOWN_ERROR);
+              return of(profile);
+            }),
+            finalize(() => this.loading$$.next(false)),
+          );
         }
+
+        return of(profile);
       }),
     );
   }
