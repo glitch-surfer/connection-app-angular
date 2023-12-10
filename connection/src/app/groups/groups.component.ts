e73ec0cc2501c73212bb9403b49d17cdb598b8a6
vladislav-garaev <marker661@gmail.com>
2023-12-10 17:19:37 +0400

    feat: add people list component

--- connection/src/app/groups/groups.component.ts
+++ connection/src/app/groups/groups.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GroupsListComponent } from './groups-list/groups-list.component';
+import { PeopleListComponent } from './people-list/people-list.component';
 
 @Component({
   selector: 'app-groups',
   standalone: true,
-  imports: [CommonModule, GroupsListComponent],
+  imports: [CommonModule, GroupsListComponent, PeopleListComponent],
   templateUrl: './groups.component.html',
   styleUrl: './groups.component.scss',
 })
