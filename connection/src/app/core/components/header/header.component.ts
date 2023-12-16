67d22af5abfe25de009a352309d2185526af6a64
vladislav-garaev <marker661@gmail.com>
2023-12-16 14:38:33 +0400

    feat: add header layout

--- connection/src/app/core/components/header/header.component.ts
+++ connection/src/app/core/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../../../api/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
