870d4af9fe18cc2ac9fd1ec528ddff95437a2cfb
vladislav-garaev <marker661@gmail.com>
2023-12-16 13:18:38 +0400

    feat: add not found message & links

--- connection/src/app/not-found/not-found.component.ts
+++ connection/src/app/not-found/not-found.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../api/auth.service';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  isAuth = AuthService.isAuth();
+
+  defaultPage = this.isAuth ? '/' : '/signin';
+}
