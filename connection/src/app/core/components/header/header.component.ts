e3301e98dc80ee7d4bb71fdcb33595139b6c4040
vladislav-garaev <marker661@gmail.com>
2023-12-16 17:29:03 +0400

    feat: save theme in LS

--- connection/src/app/core/components/header/header.component.ts
+++ connection/src/app/core/components/header/header.component.ts
@@ -14,7 +14,12 @@ import { AuthService } from '../../../api/auth.service';
 export class HeaderComponent {
   private authService = AuthService;
 
-  darkTheme = true;
+  darkTheme: boolean;
+
+  constructor() {
+    this.darkTheme = localStorage.getItem('theme') === 'dark';
+    document.body.classList.toggle('dark-theme', this.darkTheme);
+  }
 
   get isAuth() {
     return this.authService.isAuth();
@@ -24,5 +29,6 @@ export class HeaderComponent {
     this.darkTheme = !this.darkTheme;
 
     document.body.classList.toggle('dark-theme');
+    localStorage.setItem('theme', this.darkTheme ? 'dark' : '');
   }
 }
